bae4c73558dbaefc4fc4942a578f59ccf6e964db
Nikos Skliris <nikoskliris@gmail.com>
2023-11-14 16:34:25 +0200

    1.14.06 Plane Model

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { useRef } from "react";
 import planeScene from "../assets/3d/plane.glb";
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
 const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
   const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
 
   return (
     <mesh {...props}>
